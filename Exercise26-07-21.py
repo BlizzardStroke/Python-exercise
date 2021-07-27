71623757255e2658752be855c647db21673e8d5d
BlizzardStroke <ds.luis@hotmail.com>
2021-07-27 10:58:16 -0500

    Basic and loops v1.0.1

--- Exercise26-07-21.py
+++ Exercise26-07-21.py
@@ -5,13 +5,13 @@ def for_loop():
 
 
 def odd_number():
-    numbers = [1,2,3,4,5,6,7,8,9,10,11,12]
     count = 0
-    for i in numbers:
-        if(i%2 == 0 and count < 4): 
+    for number in range(1,10,2):
+        if(count < 4): 
             count = count + 1
-            print(i)
+            print(number)
     print(f"We have {count} even numbres")
 
 
 
+odd_number()
